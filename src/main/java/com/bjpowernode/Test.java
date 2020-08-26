1f880539594054fb5e95625c1d354aef96bb34dd
yangxu <474750252@qq.com>
2020-08-26 16:57:24 +0800

    修改了哈喽

---
+++ src/main/java/com/bjpowernode/Test.java
@@ -0,0 +1,12 @@
+package com.bjpowernode;
+
+/**
+ * 杨旭
+ * 2020/8/26
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("哈喽");
+        System.out.println("");
+    }
+}
